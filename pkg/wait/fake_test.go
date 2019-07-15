0900e7ac393ff712e6c9c8309c3a6c1f4a0b041a
martinohmann <martinohmann@gmail.com>
2019-07-15 20:43:03 +0200

    add coverage for fakes (#21)

---
+++ pkg/wait/fake_test.go
@@ -0,0 +1,31 @@
+package wait
+
+import (
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestFakeWaiter_Wait(t *testing.T) {
+	w := NewFakeWaiter()
+
+	r1 := &Request{
+		Options: Options{Timeout: 1 * time.Second},
+	}
+
+	err := w.Wait(r1)
+
+	require.NoError(t, err)
+
+	r2 := &Request{}
+
+	err = w.Wait(r2)
+
+	require.NoError(t, err)
+
+	expectedRequests := []*Request{r1, r2}
+
+	assert.Equal(t, expectedRequests, w.Requests)
+}
