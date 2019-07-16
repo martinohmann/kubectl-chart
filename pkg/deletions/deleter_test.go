e7eadcf9292d4cb16d51aa846aaba6b7e2bb06b3
Martin Ohmann <martin.ohmann@bonial.com>
2019-07-16 18:27:19 +0200

    add edge case test for NotFoundError on last visited info

--- pkg/deletions/deleter_test.go
+++ pkg/deletions/deleter_test.go
@@ -155,6 +155,51 @@ func TestDeleter_Delete(t *testing.T) {
 			},
 		},
 		{
+			name: "should ignore NotFound errors on last visited info",
+			infos: []*resource.Info{
+				{
+					Mapping: &meta.RESTMapping{
+						Resource: schema.GroupVersionResource{Group: "group", Version: "version", Resource: "theresource"},
+					},
+					Name:      "name-foo",
+					Namespace: "ns-foo",
+				},
+				{
+					Mapping: &meta.RESTMapping{
+						Resource: schema.GroupVersionResource{Group: "group", Version: "version", Resource: "theresource"},
+					},
+					Name:      "name-bar",
+					Namespace: "ns-bar",
+				},
+			},
+			fakeClient: func() *dynamicfakeclient.FakeDynamicClient {
+				fakeClient := dynamicfakeclient.NewSimpleDynamicClient(scheme)
+				fakeClient.PrependReactor("delete", "theresource", func(action clienttesting.Action) (handled bool, ret runtime.Object, err error) {
+					deleteAction := action.(clienttesting.DeleteAction)
+
+					if deleteAction.GetName() == "name-bar" {
+						return true, newUnstructuredList(newUnstructured("group/version", "TheKind", "ns-foo", "name-foo")), apierrors.NewNotFound(schema.GroupResource{Group: "group", Resource: "theresource"}, "name-foo")
+					}
+
+					return true, newUnstructuredList(newUnstructured("group/version", "TheKind", "ns-bar", "name-bar")), nil
+				})
+				return fakeClient
+			},
+
+			validateActions: func(t *testing.T, actions []clienttesting.Action) {
+				if len(actions) != 2 {
+					t.Fatal(spew.Sdump(actions))
+				}
+				if !actions[0].Matches("delete", "theresource") || actions[0].(clienttesting.DeleteAction).GetName() != "name-foo" {
+					t.Error(spew.Sdump(actions))
+				}
+
+				if !actions[1].Matches("delete", "theresource") || actions[1].(clienttesting.DeleteAction).GetName() != "name-bar" {
+					t.Error(spew.Sdump(actions))
+				}
+			},
+		},
+		{
 			name: "aborts deletion for errors different from NotFoundError",
 			infos: []*resource.Info{
 				{
