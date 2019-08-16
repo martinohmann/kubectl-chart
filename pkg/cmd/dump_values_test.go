8ba8a38114387ec5340cd9969d04eee7a4669dbe
martinohmann <martinohmann@gmail.com>
2019-08-16 16:05:14 +0200

    increase coverage for chart filter and errors (#35)

--- pkg/cmd/dump_values_test.go
+++ pkg/cmd/dump_values_test.go
@@ -6,14 +6,17 @@ import (
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"k8s.io/cli-runtime/pkg/genericclioptions"
+	cmdtesting "k8s.io/kubectl/pkg/cmd/testing"
 )
 
 func TestDumpValuesCmd(t *testing.T) {
+	cmdtesting.InitTestErrorHandler(t)
+
 	streams, _, buf, _ := genericclioptions.NewTestIOStreams()
 
 	cmd := NewDumpValuesCmd(streams)
 
-	cmd.SetArgs([]string{"-f", "../chart/testdata/valid-charts/chart1"})
+	cmd.Flags().Set("chart-dir", "../chart/testdata/valid-charts/chart1")
 
 	err := cmd.Execute()
 
@@ -50,11 +53,14 @@ tolerations: []
 }
 
 func TestDumpValuesCmd_Recursive(t *testing.T) {
+	cmdtesting.InitTestErrorHandler(t)
+
 	streams, _, buf, _ := genericclioptions.NewTestIOStreams()
 
 	cmd := NewDumpValuesCmd(streams)
 
-	cmd.SetArgs([]string{"-f", "../chart/testdata/valid-charts", "-R"})
+	cmd.Flags().Set("chart-dir", "../chart/testdata/valid-charts")
+	cmd.Flags().Set("recursive", "true")
 
 	err := cmd.Execute()
 
@@ -90,18 +96,10 @@ tolerations: []
 ---
 affinity: {}
 fullnameOverride: ""
-hookType: post-apply
 image:
   pullPolicy: IfNotPresent
   repository: nginx
   tag: stable
-ingress:
-  annotations: {}
-  enabled: false
-  hosts:
-  - host: chart-example.local
-    paths: []
-  tls: []
 nameOverride: ""
 nodeSelector: {}
 replicaCount: 1
@@ -113,4 +111,57 @@ tolerations: []
 `
 
 	assert.Equal(t, expected, buf.String())
+}
+
+func TestDumpValuesCmd_Filter(t *testing.T) {
+	cmdtesting.InitTestErrorHandler(t)
+
+	streams, _, buf, _ := genericclioptions.NewTestIOStreams()
+
+	cmd := NewDumpValuesCmd(streams)
+
+	cmd.Flags().Set("chart-dir", "../chart/testdata/valid-charts")
+	cmd.Flags().Set("recursive", "true")
+	cmd.Flags().Set("chart-filter", "chart2")
+
+	err := cmd.Execute()
+
+	require.NoError(t, err)
+
+	expected := `---
+# Merged values for chart: chart2
+---
+affinity: {}
+fullnameOverride: ""
+image:
+  pullPolicy: IfNotPresent
+  repository: nginx
+  tag: stable
+nameOverride: ""
+nodeSelector: {}
+replicaCount: 1
+resources: {}
+service:
+  port: 80
+  type: ClusterIP
+tolerations: []
+`
+
+	assert.Equal(t, expected, buf.String())
+}
+
+func TestDumpValuesOptions_Error(t *testing.T) {
+	streams, _, buf, _ := genericclioptions.NewTestIOStreams()
+
+	o := NewDumpValuesOptions(streams)
+	o.ChartFlags.ChartDir = "../chart/testdata"
+
+	require.NoError(t, o.Complete())
+
+	err := o.Run()
+
+	_ = buf
+
+	require.Error(t, err)
+	assert.Contains(t, err.Error(), "no Chart.yaml exists in directory")
 }
